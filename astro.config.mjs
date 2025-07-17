564f465c10f7c1e335bd087ff33f1fe7b862497d
SengMitnick <sengmitnick@163.com>
2025-07-17 23:51:22 +0800

    更新 astro.config.mjs 文件，添加 node 适配器配置；在 package.json 中添加 @astrojs/node 依赖，并更新 pnpm-lock.yaml 以反映依赖关系的变化。

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Rem 记忆体',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/ShiningAI/remembrance-web' }],
-		}),
+  integrations: [
+      starlight({
+          title: 'Rem 记忆体',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/ShiningAI/remembrance-web' }],
+      }),
 	],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
