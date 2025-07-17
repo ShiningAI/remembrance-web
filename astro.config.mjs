fb3b82f0292805f1bf9d7718baf2527ef47c796f
ray <haozanlawyer@gmail.com>
2025-07-17 18:11:27 +0800

    添加 Dockerfile、.dockerignore 文件和 GitHub Actions 工作流以支持生产环境的构建和部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Rem 记忆体',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/ShiningAI/remembrance-web' }],
 		}),
 	],
 });
